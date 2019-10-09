d0592fe3fd21c1dc77b95fb0d15d7342aa5d84e1
OscarCurtoC <oscar.curto@hotmail.com>
2019-10-09 18:54:55 +0200

    MUSICA MAS ANDAR AGUA

--- FakeContra/Player.cpp
+++ FakeContra/Player.cpp
@@ -22,7 +22,7 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
 	spritesheet.loadFromFile("images/player.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(48, 32), glm::vec2(0.1666, 0.25), &spritesheet, &shaderProgram);
+	sprite = Sprite::createSprite(glm::ivec2(48, 48), glm::vec2(0.1666, 0.25), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(6);
 	
 		sprite->setAnimationSpeed(STAND_LEFT, 8);
